649dd1464023c234dcb40e17610af640f4d3b0ac
CBenni <cbenni@streamelements.com>
2020-02-02 12:31:11 +0100

    Fixed and updated deps

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export default {
   baseUrl: 'https://api.twitch.tv/gql',
   headers: {
-    'Client-ID': 'e87kl9ovt0oxhgqdw3x5z0a6cjuen4'
+    'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
   }
 };
